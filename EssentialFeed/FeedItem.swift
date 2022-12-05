27eb98381926ac6bae48de5be5350ea23ff82f22
Heber Raziel Alvarez Ruedas <heber.alvarez@elektra.com.mx>
2022-12-05 13:05:51 -0600

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Heber Raziel Alvarez Ruedas on 05/12/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
