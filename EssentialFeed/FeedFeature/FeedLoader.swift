32f9a45bde755ed4c7846e182b8db4419a623b08
Heber Raziel Alvarez Ruedas <heber.alvarez@elektra.com.mx>
2022-12-26 13:36:58 -0600

    Make 'RemoteFeedLoader' conform to 'FeedLoader' protocol

--- EssentialFeed/FeedFeature/FeedLoader.swift
+++ EssentialFeed/FeedFeature/FeedLoader.swift
@@ -7,14 +7,14 @@
 
 import Foundation
 
+protocol FeedLoader {
+    associatedtype Error: Swift.Error
+    func load(completion: @escaping(LoadFeedResult<Error>) -> Void)
+}
+
 public enum LoadFeedResult<Error: Swift.Error >  {
     case success([FeedItem])
     case failure(Error)
 }
 
 extension LoadFeedResult: Equatable where Error: Equatable {}
-
-protocol FeedLoader {
-    associatedtype Error: Swift.Error 
-    func loadItems(completion: @escaping(LoadFeedResult<Error>) -> Void)
-}
