1225b86f642fddb0a456b990c281748807dce638
Hruedas <hruedas@expediagroup.com>
2023-05-05 15:36:13 -0600

    Save command requests cache deletion

--- EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -6,16 +6,24 @@
 //
 
 import XCTest
-
-class FeedStore {
-    var deletedCachedFeedCallCount = 0
-}
+import EssentialFeed
 
 class LocalFeedLoader {
     private let store: FeedStore
 
     init(store: FeedStore) {
         self.store = store
+    }
+
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
+    }
+}
+
+class FeedStore {
+    var deletedCachedFeedCallCount = 0
+    func deleteCachedFeed() {
+        deletedCachedFeedCallCount += 1
     }
 }
 
@@ -24,5 +32,23 @@ final class CacheFeedUseCaseTests: XCTestCase {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
         XCTAssertEqual(store.deletedCachedFeedCallCount, .zero)
+    }
+
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem, uniqueItem]
+        sut.save(items)
+        XCTAssertEqual(store.deletedCachedFeedCallCount, 1)
+    }
+
+    // MARK: - Helpers
+
+    private var uniqueItem: FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageUrl: anyURL())
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
     }
 }
